3a74aa36119596b3f5a5c2181fbc439cca235011
Daniel Caraiman <danielc@moduleworks.com>
2026-02-15 18:25:32 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char* argv[]) {
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    const QUrl url(u"qrc:/qml/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
+        [url](QObject* obj, const QUrl& objUrl) {
+            if (!obj && objUrl == url) QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+
+    engine.load(url);
+    return app.exec();
+}
---
+++ src/pch.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQml/QQmlContext>
+#include <QtQuick/QQuickWindow>
+#include <memory>
+#include <string>
+#include <vector>
