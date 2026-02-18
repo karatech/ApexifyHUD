5d0b5ed9f157b960c8d281d6e39ea8ed16aa3f19
Daniel Caraiman <danielc@moduleworks.com>
2026-02-19 00:05:59 +0200

    Add custom application icon using Qt resources

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickWindow>
+#include <QIcon>
 #include "TelemetryChartData.h"
 
 int main(int argc, char* argv[]) {
 
     QApplication app(argc, argv);
 
+    app.setWindowIcon(QIcon(":/ico.png")); 
+
     QQmlApplicationEngine engine;
     TelemetryChartData telemetryChartData;
     engine.rootContext()->setContextProperty("telemetryChartData", &telemetryChartData);
     engine.loadFromModule("ApexifyHUD", "Main");
 
-    if (engine.rootObjects().isEmpty()) return -1;
+    if (engine.rootObjects().isEmpty()) return -1; 
 
     telemetryChartData.start();
     return app.exec();
